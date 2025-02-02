38902694ab8eebcfd555194383614f76036dfe4f
derwolz <der.wolz@gmail.com>
2025-02-02 13:23:53 -0800

    Cors is retarded

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -25,5 +25,11 @@ CREATE TABLE IF NOT EXISTS email_subscriptions (
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   event.locals.db = db;
-  return resolve(event);
+  const response = await resolve(event);
+   response.headers.append('Access-Control-Allow-Origin', '*');
+  response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  response.headers.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  return response;
 }
+
